061de3a46d8bd8074a7bdf492f3e58d8fdda491b
Kevin Groat <apps@kevingroat.io>
2018-04-05 18:03:13 -0400

    Adding binary content-type wildcard support

--- test/is-binary.js
+++ test/is-binary.js
@@ -14,6 +14,22 @@ describe('is-binary', function() {
     expect(result).to.be.true;
   });
 
+  it('handles wildcards', function() {
+    const result = isBinary({ ['content-type']: 'image/png' }, {
+      binary: ['image/*']
+    });
+
+    expect(result).to.be.true;
+  });
+
+  it('does not incorrectly handle wildcards', function() {
+    const result = isBinary({ ['content-type']: 'application/json' }, {
+      binary: ['image/*']
+    });
+
+    expect(result).to.be.false;
+  });
+
   it('force to false', function() {
     const result = isBinary({}, {
       binary: false
