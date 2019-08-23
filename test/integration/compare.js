f29e98be91533a6e45e7f14073631419243cf939
Doug Moscrop <doug.moscrop@gmail.com>
2019-08-23 17:21:23 -0400

    tweak the binary stuff a bit more

--- test/integration/compare.js
+++ test/integration/compare.js
@@ -4,16 +4,12 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports.handler = async (event) => {
-  const expected = fs.readFileSync(path.join(__dirname, 'image.png'));
+  const expected = new Buffer(fs.readFileSync(path.join(__dirname, 'test.xlsx'), { encoding: 'binary' }));
+
   const { body } = event;
 
-  const actual = Buffer.isBuffer(body)
-    ? body
-    : Buffer.from(
-        typeof body === 'string'
-          ? body
-          : JSON.stringify(body)
-      );
+  const actualBinary = Buffer.from(body, 'binary');
+  const actualUtf8 = Buffer.from(body, 'utf8');
 
   return {
     statusCode: 200,
@@ -21,9 +17,15 @@ module.exports.handler = async (event) => {
       'content-type': 'application/json'
     },
     body: JSON.stringify({
-      actualBytes: Buffer.byteLength(actual),
       expectedBytes: Buffer.byteLength(expected),
-      bytesEqual: actual.equals(expected),
+      binary: {
+        actualBytes: Buffer.byteLength(actualBinary),
+        equal: expected.equals(actualBinary),
+      },
+      utf8: {
+        actualBytes: Buffer.byteLength(actualUtf8),
+        equal: expected.equals(actualUtf8),
+      },
     }),
   };
 };
