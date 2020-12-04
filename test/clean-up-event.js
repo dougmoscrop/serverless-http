80cc976860a1a98d9eaa3ed1dd49cce4b7314ccd
Jim Heising <jheising@gmail.com>
2020-12-04 10:24:05 -0800

    Support for urldecoding of query params in AWS Elastic Load Balancer payload

--- test/clean-up-event.js
+++ test/clean-up-event.js
@@ -161,4 +161,182 @@ describe('clean up event', () => {
     });
 
   });
+
+  it('should properly urldecode ELB payload query params', () => {
+    // Construct dummy v2 event
+    const v2Event = {
+      version: '2.0',
+      routeKey: '$default',
+      rawPath: '/my/path',
+      rawQueryString: 'parameter%231=value%231&parameter%231=value%232&parameter2=value',
+      cookies: ['cookie1', 'cookie2'],
+      headers: {
+        'Header1': 'value1',
+        'Header2': 'value2'
+      },
+      queryStringParameters: { 'parameter%231': 'value%231,value%232', 'parameter2': 'value' },
+      requestContext: {
+        accountId: '123456789012',
+        apiId: 'api-id',
+        authorizer: {
+          jwt: {
+            claims: { 'claim1': 'value1', 'claim2': 'value2' },
+            scopes: ['scope1', 'scope2']
+          }
+        },
+        domainName: 'id.execute-api.us-east-1.amazonaws.com',
+        domainPrefix: 'id',
+        http: {
+          method: 'POST',
+          path: '/my/path',
+          protocol: 'HTTP/1.1',
+          sourceIp: 'IP',
+          userAgent: 'agent'
+        },
+        requestId: 'id',
+        routeKey: '$default',
+        stage: '$default',
+        time: '12/Mar/2020:19:03:58 +0000',
+        timeEpoch: 1583348638390,
+        elb: {
+          targetGroupArn: "arn:aws:elasticloadbalancing:region:123456789012:targetgroup/my-target-group/6d0ecf831eec9f09"
+        }
+      },
+      body: 'Hello from Lambda',
+      pathParameters: { 'parameter1': 'value1' },
+      isBase64Encoded: false,
+      stageVariables: { 'stageVariable1': 'value1', 'stageVariable2': 'value2' }
+    };
+
+    // Clean the event
+    cleanUpEvent(v2Event, { basePath: '/my' });
+
+    expect(v2Event).to.eql({
+      version: '2.0',
+      routeKey: '$default',
+      rawPath: '/path',
+      rawQueryString: 'parameter%231=value%231&parameter%231=value%232&parameter2=value',
+      cookies: ['cookie1', 'cookie2'],
+      headers: { Header1: 'value1', Header2: 'value2' },
+      queryStringParameters: { 'parameter#1': 'value#1,value#2', parameter2: 'value' },
+      requestContext: {
+        accountId: '123456789012',
+        apiId: 'api-id',
+        authorizer: {
+          jwt: {
+            claims: { 'claim1': 'value1', 'claim2': 'value2' },
+            scopes: ['scope1', 'scope2']
+          }
+        },
+        domainName: 'id.execute-api.us-east-1.amazonaws.com',
+        domainPrefix: 'id',
+        http: {
+          method: 'POST',
+          path: '/my/path',
+          protocol: 'HTTP/1.1',
+          sourceIp: 'IP',
+          userAgent: 'agent'
+        },
+        requestId: 'id',
+        routeKey: '$default',
+        stage: '$default',
+        time: '12/Mar/2020:19:03:58 +0000',
+        timeEpoch: 1583348638390,
+        elb: {
+          targetGroupArn: "arn:aws:elasticloadbalancing:region:123456789012:targetgroup/my-target-group/6d0ecf831eec9f09"
+        }
+      },
+      body: 'Hello from Lambda',
+      pathParameters: { parameter1: 'value1' },
+      isBase64Encoded: false,
+      stageVariables: { stageVariable1: 'value1', stageVariable2: 'value2' }
+    });
+
+  });
+
+  it('should not urldecode query params for non ELB payloads', () => {
+    // Construct dummy v2 event
+    const v2Event = {
+      version: '2.0',
+      routeKey: '$default',
+      rawPath: '/my/path',
+      rawQueryString: 'parameter%231=value%231&parameter%231=value%232&parameter2=value',
+      cookies: ['cookie1', 'cookie2'],
+      headers: {
+        'Header1': 'value1',
+        'Header2': 'value2'
+      },
+      queryStringParameters: { 'parameter%231': 'value%231,value%232', 'parameter2': 'value' },
+      requestContext: {
+        accountId: '123456789012',
+        apiId: 'api-id',
+        authorizer: {
+          jwt: {
+            claims: { 'claim1': 'value1', 'claim2': 'value2' },
+            scopes: ['scope1', 'scope2']
+          }
+        },
+        domainName: 'id.execute-api.us-east-1.amazonaws.com',
+        domainPrefix: 'id',
+        http: {
+          method: 'POST',
+          path: '/my/path',
+          protocol: 'HTTP/1.1',
+          sourceIp: 'IP',
+          userAgent: 'agent'
+        },
+        requestId: 'id',
+        routeKey: '$default',
+        stage: '$default',
+        time: '12/Mar/2020:19:03:58 +0000',
+        timeEpoch: 1583348638390
+      },
+      body: 'Hello from Lambda',
+      pathParameters: { 'parameter1': 'value1' },
+      isBase64Encoded: false,
+      stageVariables: { 'stageVariable1': 'value1', 'stageVariable2': 'value2' }
+    };
+
+    // Clean the event
+    cleanUpEvent(v2Event, { basePath: '/my' });
+
+    expect(v2Event).to.eql({
+      version: '2.0',
+      routeKey: '$default',
+      rawPath: '/path',
+      rawQueryString: 'parameter%231=value%231&parameter%231=value%232&parameter2=value',
+      cookies: ['cookie1', 'cookie2'],
+      headers: { Header1: 'value1', Header2: 'value2' },
+      queryStringParameters: { 'parameter%231': 'value%231,value%232', 'parameter2': 'value' },
+      requestContext: {
+        accountId: '123456789012',
+        apiId: 'api-id',
+        authorizer: {
+          jwt: {
+            claims: { 'claim1': 'value1', 'claim2': 'value2' },
+            scopes: ['scope1', 'scope2']
+          }
+        },
+        domainName: 'id.execute-api.us-east-1.amazonaws.com',
+        domainPrefix: 'id',
+        http: {
+          method: 'POST',
+          path: '/my/path',
+          protocol: 'HTTP/1.1',
+          sourceIp: 'IP',
+          userAgent: 'agent'
+        },
+        requestId: 'id',
+        routeKey: '$default',
+        stage: '$default',
+        time: '12/Mar/2020:19:03:58 +0000',
+        timeEpoch: 1583348638390
+      },
+      body: 'Hello from Lambda',
+      pathParameters: { parameter1: 'value1' },
+      isBase64Encoded: false,
+      stageVariables: { stageVariable1: 'value1', stageVariable2: 'value2' }
+    });
+
+  });
 });
